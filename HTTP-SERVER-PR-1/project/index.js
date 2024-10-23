92dc241abd732006ddc7a38246cbea557b770e75
Dhruvik Variya <dhruvikvariya@gmail.com>
2024-10-23 16:35:09 +0530

    NODE

--- HTTP-SERVER-PR-1/project/index.js
+++ HTTP-SERVER-PR-1/project/index.js
@@ -3,29 +3,39 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url == "/"){
-        fs.readfile("index.html", "utf8", (err, data) => {
+        fs.readFile("index.html", "utf8", (err, data) => {  // Corrected the typo here
             if(err){
-                console.log(err);
+                res.writeHead(500, {"Content-Type": "text/plain"});
+                res.end("Internal Server Error");
             }
             else{
-                res.end(data)
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.end(data);
             }
-        }) 
+        }); 
     }
     else if(req.url == "/login"){
+        res.writeHead(200, {"Content-Type": "text/html"});
         res.end("User login Successfully !!");
     }
     else if(req.url == "/signup"){
+        res.writeHead(200, {"Content-Type": "text/html"});
         res.end("User Signup Successfully !!");
     }
     else if(req.url == "/contact"){
-        res.end("contact Successfully !!");
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end("Contact Successfully !!");
     }
-    else if(req.url == "/services"){
-        res.end("services Successfully !!");
+    else if(req.url == "/service"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end("Services Successfully !!");
     }
-})
+    else {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("404 Not Found");
+    }
+});
 
 server.listen(8090, () => {
-    console.log("Server is running on 8090");
-})
+    console.log("Server is running on port 8090");
+});
